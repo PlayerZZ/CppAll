def83dde53fedff670fd3bb6ee3f4f7ca550fcfc
13608082268 <1993110.>
2021-01-06 20:45:07 +0800

    完成了c++ primer 第二章的代码内容

--- CppAll/cpp_primer.h
+++ CppAll/cpp_primer.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include "other1.h"
+#include <fstream>
 using namespace std;
 inline void chapter1() {
 	
@@ -55,6 +56,25 @@ inline void chapter1() {
 		std::cout << "booka " << (booka.isbn == bookb.isbn ? "equals" : "not equals") << " bookb" << std::endl;
 	}
 }
+
+struct Sale_data
+{
+	string bookNo;
+	//unsigned units_sold = 0;//类内初始值
+	//unsigned units_sold(0);//错误方式 可以用{}
+	unsigned units_sold{ 0 };//类内初始值,这里只是范围变化并没有精度丢失 所以也不报错
+	double revenue = 0.0;
+	friend ostream &operator<<(ostream &output, const Sale_data& sale_data)
+	{
+		output << sale_data.bookNo <<" "<<sale_data.units_sold<<" "<<sale_data.revenue;
+		return output;
+	}
+	friend istream &operator >> (istream& input, Sale_data& sale_data)
+	{
+		input >> sale_data.bookNo >> sale_data.units_sold >> sale_data.revenue;
+		return input;
+	}
+};
 inline void chapter2(){
 	//2.1 base type
 	{
@@ -84,8 +104,9 @@ inline void chapter2(){
 		//float v3{ 1.1 };//会有警告因为精度缺失
 		double v3{ 1.3 };
 		//定义在函数内部的变量 不会被初始化为0
-		int o;
-		std::cout << "o value:" << o << std::endl;
+		//会报警告
+// 		int o;
+// 		std::cout << "o value:" << o << std::endl;
 		//申明和定义分离
 		extern int i;//说明我要用其他地方的i
 		//这里的i不用extern也可以直接用是因为直接就在文件里面 如果是在dll 动态库里面就不行了
@@ -115,6 +136,8 @@ inline void chapter2(){
 		const int &b = a;
 		//指针和const
 		//int *const c1 = &a;// 错误 这里加const 只是说这个对象不可修改为其他值
+		//底层const 所以底层其实“更顶层”？//所以是const 修饰其左边的东西
+		int * const const_diceng = &a1;
 		//顶层const
 		int const* c1 = &a;//这个指针的指向是可以修改的
 		int const* const c2 = &a;//绑定死了 也不可修改
@@ -127,6 +150,48 @@ inline void chapter2(){
 	}
 	//2.5数据处理
 	{
-
+		//类型别名 type alias
+		typedef double wages;
+		typedef wages base,*p;
+		{
+			//等效于
+			typedef double wages, base, *p;
+		}
+		{
+			//等价于
+			using wages = double;
+		}
+		//指针就有意思了 首先p是指针 const 只会修饰它 而不是修饰它的底层
+		double d1 = 12.4;
+		const p p1= &d1;
+		const p *p2 = &p1;
+		
+		//auto 对象 可以通过这种方式查看字面量类型
+		const auto &auto_value = 214;//因为后面其实创建了这个临时常量 来保存这个字面量
+		//decltype 类型推断
+		decltype(1) x;
+		//decltype([=] {return 1; }) x;//看来并不能通过lambda 推断返回值啊
+		auto func1 = [&](int x1) -> decltype(x1){return 1; };//通过类型推断指定返回类型
+		//对诶 为啥指针的取值叫做 “解引用”而不是叫“解指针”呢?
+		//decltype(*p) 里面的解引用操作之后变成了一个 引用！ 而不是值类型
+		decltype((d1)) dd1 = d1;//双重括号比为引用
 	}
+	//2.6 自定义数据类型
+	{
+		
+		//定义完成后有分号 因为友元的原因，定义在上面了
+		Sale_data  sale_data1,sale_data2; 
+		ifstream fin("sale_data.txt");
+		ofstream fout("sale_data2.txt");
+		fin >> sale_data1;
+		fout << sale_data1;
+	}
+	//2.7 定义自己的头文件
+#ifndef XXX_XXX_H
+#define XXX_XXX_H
+	//编写自己的头文件 这个就是简单的防重定义的保护
+	//预处理器变量 无视作用域规则 其实只是说包含范围内就有效，相当于是这个文件的全局变量范围，不是说这个就真的全局了，还是要包含才有的
+#endif
 }
+
+
